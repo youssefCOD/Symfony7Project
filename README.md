a2b009d6c9d09bb643ca46cec6d97f7a791d60d0
youssefCOD <youssefamhendpro@gmail.com>
2024-12-25 23:36:15 +0100

    finished with the database creation and migration

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-the big commandes:
+# the big commandes:
     to create a new controller: **php bin/console make:controller homepage**
     to create a new entity: **symfony console make:entity**
     to create Relations , in the entity type relations and it will allow you to make a relation 
+
+## database:
+    symfony console make:migration  -> this simply makes a migration file that describes what will be done to the db and what are the new changes
     
 
 
---
+++ migrations/Version20241225212418.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241225212418 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id SERIAL NOT NULL, parent_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, slug VARCHAR(90) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_64C19C1727ACA70 ON category (parent_id)');
+        $this->addSql('CREATE TABLE comments (id SERIAL NOT NULL, comments_id INT DEFAULT NULL, users_id INT NOT NULL, posts_id INT NOT NULL, content TEXT NOT NULL, is_reply BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_5F9E962A63379586 ON comments (comments_id)');
+        $this->addSql('CREATE INDEX IDX_5F9E962A67B3B43D ON comments (users_id)');
+        $this->addSql('CREATE INDEX IDX_5F9E962AD5E258C5 ON comments (posts_id)');
+        $this->addSql('CREATE TABLE keywords (id SERIAL NOT NULL, name VARCHAR(50) NOT NULL, slug VARCHAR(90) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE posts (id SERIAL NOT NULL, users_id INT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content TEXT NOT NULL, featured_image VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_885DBAFA67B3B43D ON posts (users_id)');
+        $this->addSql('CREATE TABLE posts_category (posts_id INT NOT NULL, category_id INT NOT NULL, PRIMARY KEY(posts_id, category_id))');
+        $this->addSql('CREATE INDEX IDX_DC83C108D5E258C5 ON posts_category (posts_id)');
+        $this->addSql('CREATE INDEX IDX_DC83C10812469DE2 ON posts_category (category_id)');
+        $this->addSql('CREATE TABLE posts_keywords (posts_id INT NOT NULL, keywords_id INT NOT NULL, PRIMARY KEY(posts_id, keywords_id))');
+        $this->addSql('CREATE INDEX IDX_70906D97D5E258C5 ON posts_keywords (posts_id)');
+        $this->addSql('CREATE INDEX IDX_70906D976205D0B8 ON posts_keywords (keywords_id)');
+        $this->addSql('CREATE TABLE users (id SERIAL NOT NULL, nickname VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_NICKNAME ON users (nickname)');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A63379586 FOREIGN KEY (comments_id) REFERENCES comments (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AD5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE posts ADD CONSTRAINT FK_885DBAFA67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE posts_category ADD CONSTRAINT FK_DC83C108D5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE posts_category ADD CONSTRAINT FK_DC83C10812469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE posts_keywords ADD CONSTRAINT FK_70906D97D5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE posts_keywords ADD CONSTRAINT FK_70906D976205D0B8 FOREIGN KEY (keywords_id) REFERENCES keywords (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE category DROP CONSTRAINT FK_64C19C1727ACA70');
+        $this->addSql('ALTER TABLE comments DROP CONSTRAINT FK_5F9E962A63379586');
+        $this->addSql('ALTER TABLE comments DROP CONSTRAINT FK_5F9E962A67B3B43D');
+        $this->addSql('ALTER TABLE comments DROP CONSTRAINT FK_5F9E962AD5E258C5');
+        $this->addSql('ALTER TABLE posts DROP CONSTRAINT FK_885DBAFA67B3B43D');
+        $this->addSql('ALTER TABLE posts_category DROP CONSTRAINT FK_DC83C108D5E258C5');
+        $this->addSql('ALTER TABLE posts_category DROP CONSTRAINT FK_DC83C10812469DE2');
+        $this->addSql('ALTER TABLE posts_keywords DROP CONSTRAINT FK_70906D97D5E258C5');
+        $this->addSql('ALTER TABLE posts_keywords DROP CONSTRAINT FK_70906D976205D0B8');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE keywords');
+        $this->addSql('DROP TABLE posts');
+        $this->addSql('DROP TABLE posts_category');
+        $this->addSql('DROP TABLE posts_keywords');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
