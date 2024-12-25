8246431d4d43fa49ce130c15a4ffb4ceb2cd1206
youssefCOD <youssefamhendpro@gmail.com>
2024-12-25 21:16:10 +0100

    refrence for starting from 0

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+//this is were we get the Route
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\PostRepository;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        //we always return Response from http foundation
         return $this->render('home/index.html.twig', [
+            //variables passed for display
             'controller_name' => 'HomeController',
+            'test' => true ,
         ]);
     }
 }
